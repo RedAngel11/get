ec5f4e6da006e4cd2276df2abae20c7a22bdfd33
КсюшаИзЛинукса <ksushat75@gmail.com>
2025-09-16 15:34:42 +0300

    workeble script

--- get-led/1-1-led.py
+++ get-led/1-1-led.py
@@ -1,4 +1,14 @@
-import RPI.GPIO as ggg
+import RPi.GPIO as ggg
+import time
 
 ggg.setmode(ggg.BCM)
 
+led = 26
+ggg.setup(led, ggg.OUT)
+state = 0
+period = 1.0
+
+while True:
+    ggg.output(led, state)
+    state = not state
+    time.sleep(period)
