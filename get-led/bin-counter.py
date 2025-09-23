ab14d41c3411d5660e367203063c93f9538aee01
КсюшаИзЛинукса <ksushat75@gmail.com>
2025-09-23 15:16:42 +0300

    бин-коунтер исправлен :)

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -13,7 +13,7 @@ gg.setup(up, gg.IN)
 gg.setup(down, gg.IN)
 
 num = 0
-sleep_time = 0.2
+sleep_time = 0.5
 
 def dec(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
@@ -22,17 +22,21 @@ while True:
     for led in leds:
         if gg.input(up):
             if num < 255: num = num +1
+            if num == 255: num = 0 
+            pr = dec(num)
             print(num, dec(num))
-            gg.output(led, 1)
+            for i in range(0,8):
+                if pr[i]==1: gg.output(leds[i], 1)
+                if pr[i]==0: gg.output(leds[i], 0)
             time.sleep(sleep_time)
-            gg.output(led, 0)
-            time.sleep(sleep_time)
+
 
         if gg.input(down):
             if num> 0: num = num - 1
-            gg.output(led, 1)
+            pr = dec(num)
             print(num, dec(num))
-            time.sleep(sleep_time)
-            gg.output(led, 0)
+            for i in range(0,8):
+                if pr[i]==1: gg.output(leds[i], 1)
+                if pr[i]==0: gg.output(leds[i], 0)
             time.sleep(sleep_time)
 
