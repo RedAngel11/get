44892101413856e557b59daed95d29171db6dd17
КсюшаИзЛинукса <ksushat75@gmail.com>
2025-09-30 16:53:22 +0300

    started 3

---
+++ get-dac/pwm_dac.py
@@ -0,0 +1,9 @@
+import GPi.GPIO as gg
+
+class PWM_DAC:
+    def __init__(self, gpio_pins, freq, range, verbose = False):
+
+    def deinit(self):
+
+    def voltage_to_quant(self, voltage):
+        
