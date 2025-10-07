dc78a6453888debadaf49665e9924ffae0eeba9e
Ksusha Titova <ksushat75@gmail.com>
2025-10-07 13:45:09 +0300

    added comments

--- get-dac/r2r_dac.py
+++ get-dac/r2r_dac.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as gg
 
 class R2R_DAC:
-    def __init__(self, gpio_pins, range, verbose=True):
+    def __init__(self, gpio_pins, range, verbose=True): #
         self.gpio_pins = gpio_pins
         self.range = range
         self.verbose = verbose
-        self.num_bits = len(gpio_pins)
+        self.cout_bits = len(gpio_pins)
         # Настройка GPIO
         gg.setmode(gg.BCM)
         gg.setup(self.gpio_pins, gg.OUT, initial=gg.LOW)
@@ -20,40 +20,41 @@ class R2R_DAC:
             code = 0
         elif voltage >= self.range:
             code = 255
-        else:
+        else: # наконец квантуем вводимое напряжение
             code = int((voltage / self.range) * 255)
         return code
 
     def set_output(self, code):
-        # Переводим в двоичное
-        bits = [int(bit) for bit in bin(code)[2:].zfill(self.num_bits)]
+        # Переводим в двоичное одной строкой: быстроцикл, bin двоичное, срез двух символов, доведение до 8 битного
+        bits = [int(bit) for bit in bin(code)[2:].zfill(self.cout_bits)]
+        # на выход получили массив 0 и 1, доведенный до 8 символов
 
-        # Подаем бит на пин
-        for pin, value in zip(self.gpio_pins, bits):#зип сопоставляет два
-            gg.output(pin, value)
-        if self.verbose:
+        # Подаем бит (0 или 1) на каждый пин
+        for pin, value in zip(self.gpio_pins, bits):# зип сопоставляет два массива
+            gg.output(pin, value) # это было САМОЕ ГЛАВНОЕ ДЕЙСТВИЕ - мы ВЫВЕЛИ БИТЫ НА ПИНЫ
+
+        if self.verbose: # показываем пользователю, что у нас получилось, если флаг врублен
             voltage_out = code / 255 * self.range
             print(f"→ Вых. напр.: {voltage_out:.2f} В | в квантах: {code} | в битах: {bits}")
 
 
-
-# =================== ОСНОВНАЯ ПРОГРАММА ===================
+# =================== НАЧИНАЕТСЯ САМА ПРОГРАММА ===================
 if __name__ == "__main__":
     try:
         # Создаём объект ЦАП
         dac = R2R_DAC(
-            gpio_pins=[16, 20, 21, 25, 26, 17, 27, 22],  # BCM номера пинов
-            range=3.183,  # опорное напряжение
-            verbose=True
+            gpio_pins = [16, 20, 21, 25, 26, 17, 27, 22],  # BCЕ номера пинов в нашем конкретном случае
+            range = 3.17 , # опорное напряжение
+            verbose = True # флажочек
         )
 
         while True:
-            try:
-                voltage = float(input("\nВведите напряжение от 0 до 3.183 В:  "))
+            try: # получаем напряжение от пользователя, квантуем, передаем на пины
+                voltage = float(input("\nВведите аналоговое напряжение от 0 до 3.17 В:  "))
                 code = dac.voltage_to_code(voltage)
                 dac.set_output(code)
             except ValueError:
-                print("введите число ")
+                print("введите нормальное float число - аналоговое напряжение ")
 
-    finally:
+    finally:# выключаем ЦАП
         dac.deinit()
