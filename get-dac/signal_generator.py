fbe286fb03ef1dea740fd516f97587653d5a354f
КсюшаИзЛинукса <ksushat75@gmail.com>
2025-10-14 15:12:28 +0300

    done 5 first tasks

--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -3,7 +3,7 @@ import time
 
 def get_sin_wave_A(freq, t):
     raw = np.sin(2* np.pi * freq * t)
-    norm = (raw +1)/2
+    norm = (raw + 1)/2 # нормируем из-за особеностей проги
     return norm
 def wait_for_sampling_period(samp_freq):
     period = 1.0 / samp_freq
